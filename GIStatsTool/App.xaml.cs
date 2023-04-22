5c4e6f1a44d5a747ff786dca2c3e5306775a6c8b
jtzeng <jtzeng41@gmail.com>
2023-04-22 16:47:10 +0800

    Initial MAUI template

---
+++ GIStatsTool/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace GIStatsTool;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ GIStatsTool/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace GIStatsTool;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
